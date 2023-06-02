a0674ff6bb41f687c896b5b3504a1285492ac07f
Yaroslav Dombrovskiy <Yaroslav.Dombrovskiy@toshibagcs.com>
2023-06-02 14:15:48 +0200

    Base typing for useState.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style/App.css';
 import {TodoItem} from "./components/TodoItem";
 
+// заготовка на будущею тудушку
+type ITodo = {
+  id: string,
+  title: string,
+  completed: boolean
+}
+
 function App() {
+  const [text, setSText] = useState("");
+  // если сложная структура  как обьект или массив я использую дженерик
+  const [todos, setTodos] = useState(Array<string>)
+  // тоже самое
+  // const [todos, setTodos] = useState( [""])
+  const [] = useState<[string] | null >(null)
+  // если мы работаем с обьектами то чаже всего будет такая запись для начального стейта
+  const [] = useState<ITodo | null>(null)
+  // можео так
+  // const [] = useState<ITodo>({} as ITodo)
+
   return (
     <div className="App">
       <TodoItem
